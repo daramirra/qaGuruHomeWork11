ffc467d973853d5de0a57b564caa7350fefcbce6
darina <linuxlinux>
2021-08-08 13:20:08 +0700

    added simple tests

---
+++ src/test/java/tests/simple/NegativeTests.java
@@ -0,0 +1,34 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTests {
+
+    @Test
+    void test0(){
+        assertTrue(false);
+    }
+
+    @Test
+    void test1(){
+        assertTrue(false);
+    }
+
+    @Test
+    void test2(){
+        assertTrue(false);
+    }
+
+    @Test
+    void test3(){
+        assertTrue(false);
+    }
+
+    @Test
+    void test4(){
+        assertTrue(false);
+    }
+
+}
---
+++ src/test/java/tests/simple/SkippedTests.java
@@ -0,0 +1,28 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SkippedTests {
+
+    @Test
+    @Disabled
+    void test0(){
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void test1(){
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled("Whith some reason")
+    void test2(){
+        assertTrue(true);
+    }
+
+}
