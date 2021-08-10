7b103bed74c21e5430463f7f87c1729b6dbf144a
darina <linuxlinux>
2021-08-10 23:48:19 +0700

    added allure attachments

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -25,6 +25,9 @@ public class TestBase {
     @AfterEach
     public void tearDrop(){
         Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
 }
